82f8ac2af4b852e85d69aa6a2f6d96a454436f86
Charaka V Mihiranga <charakavmihiranga@gmail.com>
2024-11-10 10:01:01 +0530

    Project initialize with hello controller

---
+++ src/main/java/com/springboot/securityPrac/HelloController.java
@@ -0,0 +1,12 @@
+package com.springboot.securityPrac;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String greet(){
+        return "Welcome to Spring Security";
+    }
+}
---
+++ src/main/java/com/springboot/securityPrac/SecurityPracApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.securityPrac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityPracApplication.class, args);
+	}
+
+}
